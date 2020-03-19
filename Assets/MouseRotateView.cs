96be71fafe9cf4c5281d281022e4b0a18242de23
lmtaek <ltaekman@gmail.com>
2020-03-19 15:25:54 -0400

    Mouse Rotation for Player View
    User can now rotate view based on mouse's position. Not the prettiest (could definitely tweak to make rotations more easy to handle), but functional. Will revisit when given more time.

---
+++ Assets/MouseRotateView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotateView : MonoBehaviour
+{
+    //public OVRPlayerController player;
+    //float speed = player.Acceleration;
+
+    void Update()
+    {
+        float mouseInput = Input.GetAxis("Mouse X");
+        if (mouseInput != null) {
+            Vector3 lookHere = new Vector3(0, mouseInput * 5f, 0);
+            transform.Rotate(lookHere);
+        }
+        
+    }
+}
