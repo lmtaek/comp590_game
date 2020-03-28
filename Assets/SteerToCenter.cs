47a89896a7c774ddc37a741195548ac9dc4008cc
lmtaek <ltaekman@gmail.com>
2020-03-28 15:06:45 -0400

    Touch-ups
    Changed location of collectibles and plane floor color for visibility. Added light, too.

--- Assets/SteerToCenter.cs
+++ Assets/SteerToCenter.cs
@@ -11,7 +11,6 @@ public class SteerToCenter : MonoBehaviour
     public Camera myCam;
     Vector3 prevForwardVector;
     float prevYawRelativeToCenter;
-    float longestDimensionOfPE = 3f;
     float decelerateThreshold = 0.13f;
     float accelerateThreshold = 0.30f;
 
@@ -28,28 +27,25 @@ public class SteerToCenter : MonoBehaviour
         Vector3 currentPosition = myCam.gameObject.transform.position;
         
         float howMuchUserRotated = angleBetweenVectors(prevForwardVector, currentForwardVector);
-        float directionUserRotated = (getDirection(currentPosition+prevForwardVector, currentPosition, currentPosition+currentForwardVector)<0)?-1:1;
+        float directionUserRotated = (d(currentPosition+prevForwardVector, currentPosition, currentPosition+currentForwardVector)<0)?-1:1;
         
         float deltaYawRelativeToCenter = prevYawRelativeToCenter - angleBetweenVectors(currentForwardVector, trackingSpace.transform.position - currentPosition);
         float distanceFromCenter = myCam.transform.localPosition.magnitude;
 
+        float longestDimensionOfPE = 3f;
+        
         float howMuchToAccelerate = ((deltaYawRelativeToCenter<0)? -decelerateThreshold : accelerateThreshold) * howMuchUserRotated * directionUserRotated * Mathf.Clamp(distanceFromCenter/longestDimensionOfPE/2, 0, 1);
         
         if (Mathf.Abs(howMuchToAccelerate) > 0) {
             trackingSpace.transform.RotateAround(currentPosition, new Vector3(0, 1, 0), howMuchToAccelerate);
         }
-        /*
-        valueCheck.text = "current forward vector: " + currentForwardVector + "\ncurrent position: " + currentPosition
-        + "\nhowMuchUserRotated: " + howMuchUserRotated + "\ndirectionUserRotated: " + directionUserRotated
-        + "\ndeltaYawRelativeToCenter: " + deltaYawRelativeToCenter + "\ndistanceFromCenter: " + distanceFromCenter 
-        + "\nhowMuchToAccelerate: " + howMuchToAccelerate;*/
 
         prevForwardVector = currentForwardVector;
         prevYawRelativeToCenter = angleBetweenVectors(currentForwardVector, trackingSpace.transform.position - currentPosition);
     }
 
     
-    float getDirection(Vector3 testPoint, Vector3 sourcePoint, Vector3 destinationPoint) {
+    float d(Vector3 testPoint, Vector3 sourcePoint, Vector3 destinationPoint) {
         float finalVal = ((testPoint.x - sourcePoint.x) * (destinationPoint.z - sourcePoint.z)) - ((testPoint.z - sourcePoint.z) * (destinationPoint.x - sourcePoint.x));
         return finalVal;
     }
